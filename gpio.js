7023bbe10c423310a3f3402807008c2d375e831b
vikaspogu <vikaspoguadf@gmail.com>
2020-09-30 20:15:38 -0500

    update pins array

--- gpio.js
+++ gpio.js
@@ -6,24 +6,7 @@ require("dotenv").config();
 // default: 38-open, 11-relay
 const openPin = process.env.OPEN_PIN || 38;
 const relayPin = process.env.RELAY_PIN || 11;
-let pins = [
-  "40",
-  "38",
-  "37",
-  "36",
-  "35",
-  "33",
-  "32",
-  "31",
-  "29",
-  "22",
-  "18",
-  "16",
-  "15",
-  "13",
-  "12",
-  "11",
-];
+let pins = ["40", "38", "37", "36", "35", "33", "32", "31", "29", "22"];
 
 rpio.open(openPin, rpio.INPUT, rpio.PULL_UP);
 rpio.open(relayPin, rpio.OUTPUT, rpio.HIGH);
