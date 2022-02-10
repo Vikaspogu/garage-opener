9e8427ffee4acb6503b6e6653c5fe01b5eb2e871
vikaspogu <vikaspoguadf@gmail.com>
2022-02-10 08:58:23 -0600

    exclude /heath endpoint

--- server.js
+++ server.js
@@ -1,68 +1,68 @@
-"use strict";
+"use strict"
 
-const express = require("express");
-const helmet = require("helmet");
-const path = require("path");
-const morgan = require("morgan");
-const client = require("prom-client");
-const logger = require("./logger");
-const notification = require("./notification");
-const { toggleRelay } = require("./gpio");
-const { getMqttBrokerStatus, garageState } = require("./pub-sub");
-require("dotenv").config();
+const express = require("express")
+const helmet = require("helmet")
+const path = require("path")
+const morgan = require("morgan")
+const logger = require("./logger")
+const notification = require("./notification")
+const { toggleRelay } = require("./gpio")
+const { getMqttBrokerStatus, garageState } = require("./pub-sub")
+require("dotenv").config()
 
-const app = express();
-const PORT = 8080;
+const app = express()
+const PORT = 8080
 
-app.use(express.json());
-app.use(helmet());
-app.use(express.static("public"));
-app.use("/assets", express.static("assets"));
+app.use(express.json())
+app.use(helmet())
+app.use(express.static("public"))
+app.use("/assets", express.static("assets"))
 app.use(
-  morgan("common", {
+  logger(config["morgan.debug.level"], {
     skip: function (req, res) {
-      return req.url == "/health";
-    },
-    skip: function (req, res) {
-      return req.url == "/metrics";
+      if (req.url == "/health") {
+        return true
+      } else {
+        return false
+      }
     },
   })
-);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+)
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
 
 function getState() {
   return {
     open: garageState(),
     notifications: notification.notificationsStatus(),
     brokerConnected: getMqttBrokerStatus(),
-  };
+  }
 }
 
 app.get("/", function (req, res) {
-  res.render("index", getState());
-});
+  res.render("index", getState())
+})
 
 app.get("/health", function (req, res) {
-  res.send("UP");
-});
+  res.send("UP")
+})
 
 app.get("/status", function (req, res) {
-  res.send(JSON.stringify(getState()));
-});
+  res.send(JSON.stringify(getState()))
+})
 
 app.post("/relay", function (req, res) {
-  logger.info("Relay toggled");
-  toggleRelay();
-  res.redirect("/");
-});
+  logger.info("Relay toggled")
+  toggleRelay()
+  res.redirect("/")
+})
 
 app.post("/toggleNotifications", function (req, res) {
-  logger.info("Notifications toggled");
-  notification.toggleNotifications();
-  res.redirect("/");
-});
+  logger.info("Notifications toggled")
+  notification.toggleNotifications()
+  res.redirect("/")
+})
 
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
+  logger.info(`Server running on port ${PORT}`)
+})
